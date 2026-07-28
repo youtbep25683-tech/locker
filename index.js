3b4f6a982ddbdc3aff742e5340260cc24e7e9777
youtbep25683-tech <youtbep25683@gmail.com>
2026-07-28 19:30:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PASSWORD = process.env.PASSWORD || "pCubeTechFirm@123";
+const PASSWORD = process.env.PASSWORD;
 
 app.get("/", (req, res) => {
     res.json({
